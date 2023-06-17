e1a869af2fbd96a40b2c7733282f06b6fde5253c
bhanudova <bhanudova03@gmail.com>
2023-06-17 16:47:34 +0530

    New Changes Added

--- components/Slidebar.tsx
+++ components/Slidebar.tsx
@@ -5,6 +5,7 @@ import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi"
 import Box from "./Box";
 import SlidebarItem from "./SlidebarItem";
+import Library from "./Library";
 
 interface SlidebarProps {
     children: React.ReactNode;
@@ -28,7 +29,7 @@ const Slidebar: React.FC<SlidebarProps> = ({ children }) => {
     ], [pathname]);
 
     return (
-        <div className="flex h-full">
+        <div className="flex h-full p-2">
             <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px]">
                 <Box>
                     <div className="flex flex-col gap-y-4 px-5 py-4">
@@ -38,10 +39,10 @@ const Slidebar: React.FC<SlidebarProps> = ({ children }) => {
                     </div>
                 </Box>
                 <Box className='overflow-y-auto h-full'>
-                    Song Library
+                    <Library />
                 </Box>
             </div>
-            <main className="h-full flex-1 overflow-y-auto py-2">
+            <main className="h-full flex-1 overflow-y-auto py-2 p-2">
                 {children}
             </main>
         </div>
