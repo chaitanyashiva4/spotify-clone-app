3faec56fa31a82c2a3ea437ff1e06fb7791fc3ba
bhanudova <bhanudova03@gmail.com>
2023-06-17 12:45:49 +0530

    Spotify-Clone

---
+++ components/SlidebarItem.tsx
@@ -0,0 +1,21 @@
+import { IconType } from "react-icons";
+import Link from 'next/link';
+import { twMerge } from "tailwind-merge"
+
+interface SlidebarItemProps {
+    icon: IconType;
+    label: string;
+    active: boolean;
+    href: string;
+}
+
+const SlidebarItem: React.FC<SlidebarItemProps> = ({icon:Icon,label,active,href}) => {
+  return (
+    <Link href={href} className={twMerge(`flex flex-auto h-auto item-center w-full gap-x-4 text-md cursor-pointer hover:text-white text-neutral-400 py-1`, active && "text-white")}>
+       <Icon size={26}/>
+       <p className="truncate w-full">{label}</p>
+    </Link>
+  )
+}
+
+export default SlidebarItem
