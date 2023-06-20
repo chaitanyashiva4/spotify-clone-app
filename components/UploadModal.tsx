a30b75b2402f1061f71a52c7700905f84c9b554e
bhanudova <bhanudova03@gmail.com>
2023-06-20 11:11:18 +0530

    New Changes

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -2,17 +2,27 @@
 
 import useUploadModal from "@/hooks/useUploadModal";
 import Modal from "./Modal"
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
 const UploadModal = () =>{
     const uploadModal = useUploadModal();
+    const {register,handleSubmit,reset} = useForm<FieldValues>({
+        defaultValues:{
+            author:'',
+            title:'',
+            song:null,
+            image:null
+        }
+    })
+
     const onChange = (open:boolean) =>{
         if(!open){
-            //Reset the form
+            reset()
             uploadModal.onClose();
         }
     }
     return(
-        <Modal title="Upload Modal Title" description="Upload Modal Description" isOpen={uploadModal.isOpen} onChange={onChange}>
+        <Modal title="Add a song" description="Upload an mp3 file" isOpen={uploadModal.isOpen} onChange={onChange}>
             Upload Content
         </Modal>
     )
